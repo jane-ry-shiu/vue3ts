aec4da6e51a5c42611bc8d28c57eaa61bbc1b24a
jane.ry.shiu <jane.ry.shiu@deltaww.com>
2024-06-13 11:32:27 +0800

    feat: add slots to stories

--- src/components/AppButton.stories.ts
+++ src/components/AppButton.stories.ts
@@ -9,6 +9,8 @@ const meta = {
   tags: ['autodocs'],
   argTypes: {},
   args: {
+    default: `Button`,
+
     // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
     onClick: fn()
   }
